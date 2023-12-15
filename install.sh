0dd6e161a85c0517fb7eb18cbaf4985c6a40c9d6
blast <rodfval.fv@gmail.com>
2023-12-15 16:53:46 +0100

    aa

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+/usr/bin/git clone https://github.com/freqtrade/freqtrade freqtrade-git
 /usr/bin/git clone https://github.com/nateemma/strategies phils_strategies
 ./update.sh
 
-docker compose build
+docker compose build --no-cache
 sudo systemctl enable docker-compose@freqtrade
 sudo systemctl start docker-compose@freqtrade
