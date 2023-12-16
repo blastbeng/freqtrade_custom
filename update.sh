5af900f39669b9dfcd4cfbb6110a5a7b2ee0056a
blast <rodfval.fv@gmail.com>
2023-12-16 22:36:26 +0100

    working on udoo

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sudo systemctl stop docker-compose@freqtrade
-
-cd ./phils_strategies
+cd ./freqtrade-git
+/usr/bin/git pull
+cd ../phils_strategies
 /usr/bin/git pull
 cd ..
 
 docker compose build
 
-sudo systemctl start docker-compose@freqtrade
+sudo systemctl restart docker-compose@freqtrade
