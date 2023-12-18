c6b092621e22d3c1956f51d477450f1f97cdcffd
blast <rodfval.fv@gmail.com>
2023-12-18 17:13:26 +0100

    fvarious fixes

--- update.sh
+++ update.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd /opt/docker/compose/freqtrade
-cd ./freqtrade-git
-/usr/bin/git pull
 cd ../phils_strategies
 /usr/bin/git pull
 cd ..
@@ -9,7 +7,5 @@ cd ..
 mkdir -p ./custom_strategies/utils/
 cp -rf ./phils_strategies/NNPredict/*.py ./custom_strategies/
 cp -rf ./phils_strategies/utils/* ./custom_strategies/utils/
-
-docker compose build
 
 sudo systemctl restart docker-compose@freqtrade
