08ef02113a0a2a37803d503ee6524183381d1018
blast <rodfval.fv@gmail.com>
2023-12-18 14:38:47 +0100

    new buildfile

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+cd /opt/docker/compose/freqtrade
 /usr/bin/git clone https://github.com/freqtrade/freqtrade freqtrade-git
 /usr/bin/git clone https://github.com/nateemma/strategies phils_strategies
 ./update.sh
+
+
+mkdir -p ./custom_strategies/models/
+cp -rf ./phils_strategies/NNPredict/models/* ./custom_strategies/models/
 
 docker compose build
 sudo cp docker-compose@.service /lib/systemd/system/docker-compose@.service
 sudo systemctl daemon-reload
 sudo systemctl enable docker-compose@freqtrade
-sudo systemctl start docker-compose@freqtrade
+sudo systemctl restart docker-compose@freqtrade
--- update.sh
+++ update.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+cd /opt/docker/compose/freqtrade
 cd ./freqtrade-git
 /usr/bin/git pull
 cd ../phils_strategies
 /usr/bin/git pull
 cd ..
+
+mkdir -p ./custom_strategies/utils/
+cp -rf ./phils_strategies/NNPredict/*.py ./custom_strategies/
+cp -rf ./phils_strategies/utils/* ./custom_strategies/utils/
 
 docker compose build
 
